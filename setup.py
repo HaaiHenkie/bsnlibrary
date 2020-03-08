a719ed0908cd2ee62a61736af68228e721ae432d
Henk van den Akker <haaihenkie@users.sourceforge.net>
2020-03-08 12:12:20 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='robotframework-bsnlibrary',
+    version='0.1',
+    packages=['BSNLibrary'],
+    url='https://github.com/HaaiHenkie/bsnlibrary',
+    license='GNU General Public License version 3',
+    author='Henk van den Akker',
+    description='Robot Framework library for generating BSNs (Dutch citizen service number)'
+)
